780499f07f13cd9826fe6f9b171b4628a3dec0a3
Arif-git <62111088+Arif-git@users.noreply.github.com>
2020-11-09 07:22:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "testinstance"{
    ami = var.ami
    instance_type = var.instance_type
    key_name = var.key
-   user_data = "${file("${var.bootstrap_path}")}"
+   
 
    tags = {
 #        Name = "Server-${count.index}"
