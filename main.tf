8666738751ae097d2ab1c09e5a7418c0d2501dec
Arif-git <62111088+Arif-git@users.noreply.github.com>
2020-11-09 16:05:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider  "aws" {
    region = var.aws_region
    version = "~> 3.0"
-   access_key = "J8rwFONCAQe0KGIUJHuB1o7866J2B+gzNTp7OOoK"
-  secret_key = "4321qwer"
+   access_key = "AKIAIDMKXKPCGRUUVUYA"
+  secret_key = "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"
 }
 
 resource "aws_instance" "testinstance"{
